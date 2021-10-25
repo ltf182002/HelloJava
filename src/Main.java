488286e74733934afe3ec90eb24b9ec47b6d268c
大飞哥 <155307642@qq.com>
2021-10-25 14:40:18 +0800

    Squashed commit of the following:
    commit e5ac081f76bab4af81256b22d644677de325ccc2
    Author: 大飞哥 <155307642@qq.com>
    Date:   Mon Oct 25 14:38:26 2021 +0800
    
        唱歌
    
    commit 81bca3a11f7a3f94eaf8a1c7e05e00b939ff3131
    Author: 大飞哥 <155307642@qq.com>
    Date:   Mon Oct 25 14:34:34 2021 +0800
    
        添加了文本文件
    
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 		System.out.println("今晚去打球");
+		System.out.println("今晚去唱歌!");
     }
 }
